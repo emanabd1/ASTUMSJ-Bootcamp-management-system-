e21491de0350135defc2f3756c1e71bcc1253696
Nesrin Mohammed <nesrinmohammed497@gmail.com>
2026-08-20 09:42:56 -0700

    fixed dashboard layout

--- client/src/components/layout/DashboardLayout.jsx
+++ client/src/components/layout/DashboardLayout.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
 export default function DashboardLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
-    <div className="flex h-screen w-screen bg-[#16110e] font-serif overflow-hidden">
-      <Sidebar />
-      <div className="flex flex-1 flex-col overflow-hidden">
-        <Navbar />
+    <div className="flex h-screen w-screen overflow-hidden bg-[#16110e] font-serif">
+      {sidebarOpen && <Sidebar />}
+
+      <div className="flex min-w-0 flex-1 flex-col overflow-hidden">
+        <Navbar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
+
         <main className="flex-1 overflow-y-auto bg-[#16110e] p-8 text-[#f5efe6]">
           <Outlet />
         </main>
